f9662181330e4da754e1b2e18e305048368bbd93
Nowa <135564143+Nowa771@users.noreply.github.com>
2024-04-04 11:53:00 +1100

    Added score
    Added score and a multiplier that increases when you get a combo and resets when you miss a note
    also added in scripts so you can get different points depending how on time your hits are.
    Added Partial effects and made prefabs to give player feedback on how intime they where

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,11 +14,26 @@ public class GameManager : MonoBehaviour
 
     public static GameManager instance;
 
+    public int currentScore;
+    public int scorePerNote = 100;
+    public int scorePerGoodNote = 125;
+    public int scorePerPerfectNote = 150;
+
+    public int currentMultiplier;
+    public int multiplierTracker;
+    public int[] multiplierThresholds;
+
+    public Text scoreText;
+    public Text multiText;
+
 
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+
+        scoreText.text = "score: 0";
+        currentMultiplier = 1;
     }
 
     // Update is called once per frame
@@ -37,11 +53,49 @@ public class GameManager : MonoBehaviour
 
     public void NoteHit()
     {
-        Debug.Log("Hit On Time");
+        //Debug.Log("Hit On Time");
+
+       if(currentMultiplier - 1 < multiplierThresholds.Length)
+        {
+
+        multiplierTracker++;
+
+        if (multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
+        {
+            multiplierTracker = 0;
+                currentMultiplier++;
+        }
+       }
+        //currentScore += scorePerNote * currentMultiplier;
+       scoreText.text = "score: " + currentScore;
+        multiText.text = "Multiplier: x" + currentMultiplier;
+    }
+    
+    public void NormalHit()
+    {
+        currentScore += scorePerNote * currentMultiplier;
+        NoteHit();
+    }
+
+    public void GoodHit()
+    {
+        currentScore += scorePerGoodNote * currentMultiplier;
+        NoteHit();
+    }
+
+    public void perfectHit()
+    {
+        currentScore += scorePerPerfectNote * currentMultiplier;
+        NoteHit();
     }
 
     public void NoteMissed()
     {
         Debug.Log("Missed Note");
+
+        currentMultiplier= 1;
+        multiplierTracker= 0;
+
+        multiText.text = "Multiplier: x" + currentMultiplier;
     }
 }
--- Assets/Scripts/NoteObject.cs
+++ Assets/Scripts/NoteObject.cs
@@ -8,6 +8,8 @@ public class NoteObject : MonoBehaviour
     public bool canBePressed;
     public KeyCode KeyToPress;
 
+    public GameObject hitEffect, goodEffect, perfectEffect, missEffect;
+
     private bool hasBeenPressed = false; // whether the key has been pressed
 
     
@@ -23,7 +25,27 @@ public class NoteObject : MonoBehaviour
         {
             hasBeenPressed = true; // key as pressed to prevent subsequent presses
             gameObject.SetActive(false);
-            GameManager.instance.NoteHit();
+            //GameManager.instance.NoteHit();
+
+            if (Mathf.Abs(transform.position.y) > 0.25)
+            {
+                Debug.Log("Hit");
+                GameManager.instance.NormalHit();
+                Instantiate(hitEffect, transform.position, hitEffect.transform.rotation);
+            }
+            else if (Mathf.Abs(transform.position.y) > 0.05f)
+            {
+                Debug.Log("Good");
+                GameManager.instance.GoodHit();
+                Instantiate(goodEffect, transform.position, goodEffect.transform.rotation);
+            }
+            else
+            {
+                Debug.Log("perfect");
+                GameManager.instance.perfectHit();
+                Instantiate(perfectEffect, transform.position, perfectEffect.transform.rotation);
+            }
+
         }
     }
 
@@ -44,6 +66,7 @@ public class NoteObject : MonoBehaviour
             if (!hasBeenPressed) // Check if the key hasn't been pressed before exiting
             {
                 GameManager.instance.NoteMissed();
+                Instantiate(missEffect, transform.position, missEffect.transform.rotation);
             }
         }
     }
